1c65b6f21fc5b6129c7462f1bb05cda034af6869
akbarkhan9044 <akbarkhan904412@gmail.com>
2026-09-04 22:45:27 +0530

    adding new commit

--- src/dashboard/Spider.jsx
+++ src/dashboard/Spider.jsx
@@ -5,6 +5,8 @@ export default function Spider() {
     <div>
       <h1>Welcome to Spider man Something is amzing</h1>
       <h1>Welcome to Spider man</h1>
+            <h1>Welcome to Spider man</h1>
+                  <h1>Welcome to Spider man</h1>
     </div>
   )
 }
