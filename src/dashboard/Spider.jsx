54ca92af0df4c331b8ffbaf64aa6be3bfde800b8
akbarkhan9044 <akbarkhan904412@gmail.com>
2026-09-04 22:40:17 +0530

    second changes

--- src/dashboard/Spider.jsx
+++ src/dashboard/Spider.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Spider() {
   return (
     <div>
-      <h1>Welcome to Spider man</h1>
+      <h1>Welcome to Spider man Something is amzing</h1>
     </div>
   )
 }
